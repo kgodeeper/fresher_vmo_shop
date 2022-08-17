618a2367089c992cbbcb866302674234a65d8328
kgodeeper <kgodeeper@gmail.com>
2022-08-17 21:29:08 +0700

    upload connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './configs/database';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'src/configs/.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: databaseConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
