7f39bf55a3acc4d5805ec98ea41e026b8e2bad50
kgodeeper <kgodeeper@gmail.com>
2022-08-19 23:12:17 +0700

    [Login] update login respone

--- src/modules/auths/auth.controller.ts
+++ src/modules/auths/auth.controller.ts
@@ -13,6 +13,6 @@ export class AuthController {
       body.account,
       body.password,
     );
-    res.status(HttpStatus.OK).json({ accessToken: token });
+    res.status(HttpStatus.OK).json(token);
   }
 }
