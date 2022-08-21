146963b44aa5e27addfc7deedbeece87b172775b
kgodeeper <kgodeeper@gmail.com>
2022-08-21 16:59:40 +0700

    [Login, Register] update validator by Joi

--- src/modules/auths/auth.module.ts
+++ src/modules/auths/auth.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { JwtConfig } from 'src/configs/jwt.config';
 import { RedisCacheModule } from '../caches/cache.module';
 import { AccountModule } from '../accounts/account.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MailModule } from '../mailer/mailer.module';
 import { JWTModule } from '../jwts/jwt.module';
+import { JoiPipeModule } from 'nestjs-joi';
 
 @Module({
-  imports: [AccountModule, RedisCacheModule, MailModule, JWTModule],
+  imports: [
+    AccountModule,
+    RedisCacheModule,
+    MailModule,
+    JWTModule,
+    JoiPipeModule,
+  ],
   exports: [],
   providers: [AuthService],
   controllers: [AuthController],
