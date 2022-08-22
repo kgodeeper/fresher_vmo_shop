fca859719f1a61062e9bba267645dcfd8439f454
kgodeeper <kgodeeper@gmail.com>
2022-08-22 15:38:31 +0700

    update utils, create string.utils

--- src/utils/code-generator.util.ts
+++
@@ -1,3 +0,0 @@
-export function generateCode(): string {
-  return Math.floor(Math.random() * 900000 + 100000) + '';
-}
--- src/utils/string.util.ts
+++ src/utils/string.util.ts
@@ -1,8 +1,20 @@
 import { ConfigService } from '@nestjs/config';
 
+export function generateCode(): string {
+  return Math.floor(Math.random() * 900000 + 100000) + '';
+}
+
 export async function encrypt(password: string): Promise<string> {
   const { createHash } = await import('crypto');
   return createHash(new ConfigService().get<string>('HASHAGR'))
     .update(password)
     .digest('hex');
+}
+
+export function splitString(
+  input: string,
+  separate: string,
+  getPosition: number,
+): string {
+  return input.split(separate).at(getPosition);
 }
