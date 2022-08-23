54bcf5867420f00fc0f76279ecb025691006f867
kgodeeper <kgodeeper@gmail.com>
2022-08-23 14:47:01 +0700

    Add format name

--- src/utils/string.util.ts
+++ src/utils/string.util.ts
@@ -1,4 +1,5 @@
 import { ConfigService } from '@nestjs/config';
+import { string } from 'joi';
 
 export function generateCode(): string {
   return Math.floor(Math.random() * 900000 + 100000) + '';
@@ -21,4 +22,17 @@ export function splitString(
 
 export function randomNumber(): string {
   return `${Math.floor(Math.random() * 1000) + Number(new Date())}`;
+}
+
+export function formatName(input: string): string {
+  let name = input.trim().split(' ');
+  name = name.reduce((prev: string[], item: string) => {
+    if (item !== '') prev.push(item);
+    return prev;
+  }, []);
+  name = name.map((item) => {
+    item.toLowerCase();
+    return item[0].toUpperCase() + item.substring(1);
+  });
+  return name.join(' ');
 }
