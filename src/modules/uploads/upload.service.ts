995b1a9eb3a56dbee388ebccdc205a34824598a2
kgodeeper <kgodeeper@gmail.com>
2022-08-31 16:23:05 +0700

    [Upload] update upload feture, upload to other folder

--- src/modules/uploads/upload.service.ts
+++ src/modules/uploads/upload.service.ts
@@ -1,17 +1,24 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { v2, UploadApiResponse, UploadApiErrorResponse } from 'cloudinary';
 import { Readable } from 'stream';
 
 @Injectable()
 export class UploadService {
+  constructor(private configService: ConfigService) {}
   async uploadImage(
     file: Express.Multer.File,
+    destination: string,
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     if (!file.mimetype.includes('image'))
       throw new BadRequestException('File extension is not allowed');
     return new Promise((resolve, reject) => {
       const upload = v2.uploader.upload_stream(
-        { folder: 'laptop_shop_uploads' },
+        {
+          folder: `${this.configService.get<string>(
+            'UPLOAD_BASE_FOLDER',
+          )}/${destination}`,
+        },
         (error, result) => {
           if (error) return reject(error);
           resolve(result);
@@ -21,14 +28,21 @@ export class UploadService {
     });
   }
 
-  async uploadToCloudinary(file: Express.Multer.File): Promise<any> {
-    return this.uploadImage(file).catch((error) => {
+  async uploadToCloudinary(
+    file: Express.Multer.File,
+    destination: string,
+  ): Promise<any> {
+    return this.uploadImage(file, destination).catch((error) => {
       if (error instanceof BadRequestException) throw error;
       else throw new BadRequestException('Upload file fail');
     });
   }
 
-  async removeFromCloudinary(url: string): Promise<void> {
-    v2.uploader.destroy(`laptop_shop_uploads/${url}`);
+  async removeFromCloudinary(url: string, destination: string): Promise<void> {
+    v2.uploader.destroy(
+      `${this.configService.get<string>(
+        'UPLOAD_BASE_FOLDER',
+      )}/${destination}/${url}`,
+    );
   }
 }
