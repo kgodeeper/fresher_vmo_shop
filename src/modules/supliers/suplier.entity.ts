20d5c4d1f1fd359a796fcd76722aa468e7910370
kgodeeper <kgodeeper@gmail.com>
2022-08-31 16:28:06 +0700

    [Suplier] add status to Entity, update suplier service

--- src/modules/supliers/suplier.entity.ts
+++ src/modules/supliers/suplier.entity.ts
@@ -1,3 +1,4 @@
+import { Status } from '../../commons/enum.common';
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
@@ -13,4 +14,11 @@ export class Suplier extends BaseEntity {
 
   @Column()
   contact: string;
+
+  @Column({
+    type: 'enum',
+    enum: Status,
+    default: Status.ACTIVE,
+  })
+  status: Status;
 }
--- src/modules/supliers/suplier.service.ts
+++ src/modules/supliers/suplier.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { ServiceUtil } from 'src/utils/service.utils';
+import { ServiceUtil } from '../../utils/service.utils';
 import { DataSource, Repository } from 'typeorm';
 import { Suplier } from './suplier.entity';
 
@@ -7,5 +7,9 @@ import { Suplier } from './suplier.entity';
 export class SuplierService extends ServiceUtil<Suplier, Repository<Suplier>> {
   constructor(private dataSource: DataSource) {
     super(dataSource.getRepository(Suplier));
+  }
+
+  async getById(id: string): Promise<Suplier> {
+    return this.repository.findOne({ where: { pkSuplier: id } });
   }
 }
